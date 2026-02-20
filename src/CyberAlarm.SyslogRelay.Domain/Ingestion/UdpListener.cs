6f0d0f11e3690db7e5c963b467467d1ec8c6aa6f
Waterstons <devops@waterstons.com>
2026-02-20 16:52:29 +0000

    Mirror from Azure DevOps [release=True]

---
+++ src/CyberAlarm.SyslogRelay.Domain/Ingestion/UdpListener.cs
@@ -0,0 +1,69 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using CyberAlarm.SyslogRelay.Common.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace CyberAlarm.SyslogRelay.Domain.Ingestion;
+
+public sealed class UdpListener(
+    IOptions<RelayOptions> options,
+    ILogger<UdpListener> logger) : IDisposable
+{
+    private readonly RelayOptions _options = options.Value;
+    private readonly ILogger<UdpListener> _logger = logger;
+
+    private UdpClient? _udpClient;
+    private CancellationTokenSource? _cts;
+
+    public async Task StartAsync(Func<SyslogEvent, CancellationToken, Task> ingestAction, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(ingestAction);
+
+        _cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken);
+
+        _logger.LogInformation("Starting UDP listener.");
+        _udpClient = new(new IPEndPoint(IPAddress.Any, _options.UdpPort));
+
+        try
+        {
+            while (!_cts.Token.IsCancellationRequested)
+            {
+                var result = await _udpClient.ReceiveAsync(_cts.Token);
+                await ingestAction.Invoke(SyslogEvent.FromUdp(result.RemoteEndPoint, result.Buffer), _cts.Token);
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            if (cancellationToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("UDP listener was cancelled.");
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Error running UDP listener.");
+        }
+        finally
+        {
+            Dispose();
+            _logger.LogInformation("UDP listener stopped.");
+        }
+    }
+
+    public void Stop()
+    {
+        _logger.LogInformation("Stopping UDP listener.");
+        Dispose();
+    }
+
+    public void Dispose()
+    {
+        _cts?.Cancel();
+        _cts?.Dispose();
+        _cts = null;
+
+        _udpClient?.Close();
+        _udpClient = null;
+    }
+}
---
+++ src/CyberAlarm.SyslogRelay.Domain/Upload/Services/ISecureUploader.cs
@@ -0,0 +1,6 @@
+﻿namespace CyberAlarm.SyslogRelay.Domain.Upload.Services;
+
+public interface ISecureUploader
+{
+    Task UploadFilesAsync(CancellationToken cancellationToken);
+}
