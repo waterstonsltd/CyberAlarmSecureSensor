2e748d30bf1ad090ffa68669b149d3e2e5067ab1
Waterstons <devops@waterstons.com>
2026-03-26 16:38:03 +0000

    Mirror from Azure DevOps [release=True]

---
+++ src/CyberAlarm.SyslogRelay.Domain.Tests/Builders/PeriodicOperationBuilder.cs
@@ -0,0 +1,19 @@
+﻿using CyberAlarm.SyslogRelay.Domain.Services;
+using Microsoft.Extensions.Logging;
+
+namespace CyberAlarm.SyslogRelay.Domain.Tests.Builders;
+
+internal sealed class PeriodicOperationBuilder
+{
+    public PeriodicOperationBuilder()
+    {
+        ApplicationManager = Substitute.For<IApplicationManager>();
+        Logger = Substitute.For<ILogger<PeriodicOperation>>();
+    }
+
+    public IApplicationManager ApplicationManager  { get; }
+
+    public ILogger<PeriodicOperation> Logger { get; }
+
+    public PeriodicOperation Build() => new(ApplicationManager, Logger);
+}
---
+++ src/CyberAlarm.SyslogRelay.Domain.Tests/Upload/Extensions/StringHostFingerprintExtensions.cs
@@ -0,0 +1,20 @@
+﻿using CyberAlarm.SyslogRelay.Domain.Upload.Extensions;
+
+namespace CyberAlarm.SyslogRelay.Domain.Tests.Upload.Extensions;
+
+public class StringHostFingerprintExtensions
+{
+    [Theory]
+    [InlineData("abc123", "abc123", true)]
+    [InlineData("abc123", "def456", false)]
+    [InlineData("abc123", "abc123=", true)]
+    [InlineData("abc123", "abc123==", true)]
+    [InlineData("abc123", "", false)]
+    [InlineData("", "abc123", false)]
+    public void IndicatesMatchingHostFingerprints(string hostFingerprint, string otherHostFingerprint, bool expected)
+    {
+        var actual = hostFingerprint.MatchesHostFingerprint(otherHostFingerprint);
+
+        Assert.Equal(expected, actual);
+    }
+}
