6b0d63abe8196c44fc89f763daae60547de4a1ab
Waterstons <devops@waterstons.com>
2026-03-31 16:54:00 +0000

    Mirror from Azure DevOps [release=True]

---
+++ src/CyberAlarm.SyslogRelay.Common/Models/ValidationStatus.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CyberAlarm.SyslogRelay.Common.Models;
+
+[JsonConverter(typeof(JsonStringEnumConverter))]
+public enum ValidationStatus
+{
+    Success,
+    UnableToPatternMatch,
+    UnableToParse,
+    LocalOnlyEvent,
+    Ignored,
+}
