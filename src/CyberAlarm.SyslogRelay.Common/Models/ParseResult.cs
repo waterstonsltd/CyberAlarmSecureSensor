5ea8f316e38b9a19f0c9293d16cc4a3b7d05d357
Waterstons <devops@waterstons.com>
2026-04-23 13:13:24 +0000

    Mirror from Azure DevOps [release=True]

---
+++ src/CyberAlarm.SyslogRelay.Common/Models/ParseResult.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CyberAlarm.SyslogRelay.Common.Models;
+
+public sealed record ParseResult(
+    string SourceIp,
+    [property: JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)] string? DestinationIp,
+    [property: JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)] int? SourcePort,
+    [property: JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)] int? DestinationPort,
+    EventProtocol Protocol,
+    EventAction Action,
+    [property: JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)] TimeSpan? Duration = null,
+    [property: JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)] long? Bytes = null);
