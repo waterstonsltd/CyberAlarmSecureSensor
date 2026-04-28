9896368a9b4dc98695d2c538b1fd3a2b705c0117
Waterstons <devops@waterstons.com>
2026-04-28 18:38:55 +0000

    Mirror from Azure DevOps [release=True]

---
+++ src/CyberAlarm.SyslogRelay.Domain.Tests/Builders/ParserConfigBuilderBase.cs
@@ -0,0 +1,122 @@
+﻿using CyberAlarm.SyslogRelay.Common.Models.ParserConfiguration;
+
+namespace CyberAlarm.SyslogRelay.Domain.Tests.Builders;
+
+internal abstract class ParserConfigBuilderBase<TParserConfig>
+    where TParserConfig : ParserConfig
+{
+    protected string[] _sourceIpKeys = [];
+    protected string[] _destinationIpKeys = [];
+    protected bool _isDestinationIpOptional;
+    protected string[] _sourcePortKeys = [];
+    protected bool _isSourcePortOptional;
+    protected string[] _destinationPortKeys = [];
+    protected bool _isDestinationPortOptional;
+    protected string[] _protocolKeys = [];
+    protected bool _isProtocolOptional;
+    protected string[] _actionKeys = [];
+    protected bool _isActionOptional;
+    protected string[] _allowActionValues = [];
+    protected string[] _denyActionValues = [];
+    protected string[] _dropActionValues = [];
+    protected string[] _closeActionValues = [];
+    protected string[] _resetActionValues = [];
+    protected string[] _timeoutActionValues = [];
+    protected string[] _durationKeys = [];
+    protected bool _durationIsSeconds;
+    protected string[] _totalBytesKeys = [];
+    protected string[] _sentBytesKeys = [];
+    protected string[] _receivedBytesKeys = [];
+
+    public abstract TParserConfig Build();
+
+    public ParserConfigBuilderBase<TParserConfig> WithSourceIpKeys(params string[] keys)
+    {
+        _sourceIpKeys = keys;
+        return this;
+    }
+
+    public ParserConfigBuilderBase<TParserConfig> WithDestinationIpKeys(params string[] keys)
+    {
+        _destinationIpKeys = keys;
+        return this;
+    }
+
+    public ParserConfigBuilderBase<TParserConfig> WithSourcePortKeys(params string[] keys)
+    {
+        _sourcePortKeys = keys;
+        return this;
+    }
+
+    public ParserConfigBuilderBase<TParserConfig> WithDestinationPortKeys(params string[] keys)
+    {
+        _destinationPortKeys = keys;
+        return this;
+    }
+
+    public ParserConfigBuilderBase<TParserConfig> WithProtocolKeys(params string[] keys)
+    {
+        _protocolKeys = keys;
+        return this;
+    }
+
+    public ParserConfigBuilderBase<TParserConfig> WithActionKeys(params string[] keys)
+    {
+        _actionKeys = keys;
+        return this;
+    }
+
+    public ParserConfigBuilderBase<TParserConfig> WithActionValues(
+        string[]? allowValues = null,
+        string[]? denyValues = null,
+        string[]? dropValues = null,
+        string[]? closeValues = null,
+        string[]? resetValues = null,
+        string[]? timeoutValues = null)
+    {
+        _allowActionValues = allowValues ?? [];
+        _denyActionValues = denyValues ?? [];
+        _dropActionValues = dropValues ?? [];
+        _closeActionValues = closeValues ?? [];
+        _resetActionValues = resetValues ?? [];
+        _timeoutActionValues = timeoutValues ?? [];
+
+        return this;
+    }
+
+    public ParserConfigBuilderBase<TParserConfig> WithOptional(
+        bool destinationIp = true,
+        bool sourcePort = true,
+        bool destinationPort = true,
+        bool protocol = true,
+        bool action = true)
+    {
+        _isDestinationIpOptional = destinationIp;
+        _isSourcePortOptional = sourcePort;
+        _isDestinationPortOptional = destinationPort;
+        _isProtocolOptional = protocol;
+        _isActionOptional = action;
+
+        return this;
+    }
+
+    public ParserConfigBuilderBase<TParserConfig> WithDurationKeys(bool isSeconds, params string[] keys)
+    {
+        _durationKeys = keys;
+        _durationIsSeconds = isSeconds;
+        return this;
+    }
+
+    public ParserConfigBuilderBase<TParserConfig> WithTotalBytesKeys(params string[] keys)
+    {
+        _totalBytesKeys = keys;
+        return this;
+    }
+
+    public ParserConfigBuilderBase<TParserConfig> WithBytesKeys(string[] sentKeys, string[] receivedKeys)
+    {
+        _sentBytesKeys = sentKeys;
+        _receivedBytesKeys = receivedKeys;
+        return this;
+    }
+}
---
+++ src/CyberAlarm.SyslogRelay.Domain.Tests/Initialisation/DeleteTemporaryFilesTests.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CyberAlarm.SyslogRelay.Domain.Initialisation;
+using CyberAlarm.SyslogRelay.Domain.Services;
+using CyberAlarm.SyslogRelay.Domain.Tests.Builders;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Xunit;
+
+namespace CyberAlarm.SyslogRelay.Domain.Tests.Initialisation;
+
+public class DeleteTemporaryFilesTests
+{
+    [Fact]
+    public async Task RunAsync_WithNoFiles_ReturnsOkResult()
+    {
+        // Arrange
+        var sut = new DeleteTemporaryFilesBuilder()
+            .WithTemporaryFolder("C:\\Temp")
+            .WithFiles()
+            .Build();
+
+        // Act
+        var result = await sut.RunAsync(CancellationToken.None);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task RunAsync_WithSingleFile_DeletesFile()
+    {
+        // Arrange
+        var fileManager = Substitute.For<IFileManager>();
+        var sut = new DeleteTemporaryFilesBuilder()
+            .WithFileManager(fileManager)
+            .WithTemporaryFolder("C:\\Temp")
+            .WithFiles("C:\\Temp\\file1.txt")
+            .Build();
+
+        // Act
+        await sut.RunAsync(CancellationToken.None);
+
+        // Assert
+        fileManager.Received(1).Delete("C:\\Temp\\file1.txt");
+    }
+
+    [Fact]
+    public async Task RunAsync_WithMultipleFiles_DeletesAllFiles()
+    {
+        // Arrange
+        var fileManager = Substitute.For<IFileManager>();
+        var sut = new DeleteTemporaryFilesBuilder()
+            .WithFileManager(fileManager)
+            .WithTemporaryFolder("C:\\Temp")
+            .WithFiles("C:\\Temp\\file1.txt", "C:\\Temp\\file2.txt", "C:\\Temp\\file3.txt")
+            .Build();
+
+        // Act
+        await sut.RunAsync(CancellationToken.None);
+
+        // Assert
+        fileManager.Received(1).Delete("C:\\Temp\\file1.txt");
+        fileManager.Received(1).Delete("C:\\Temp\\file2.txt");
+        fileManager.Received(1).Delete("C:\\Temp\\file3.txt");
+    }
+
+    [Fact]
+    public async Task RunAsync_WhenDeleteThrowsException_StillReturnsOkResult()
+    {
+        // Arrange
+        var exception = new IOException("File locked");
+        var sut = new DeleteTemporaryFilesBuilder()
+            .WithTemporaryFolder("C:\\Temp")
+            .WithFiles("C:\\Temp\\file1.txt")
+            .WithDeleteException("C:\\Temp\\file1.txt", exception)
+            .Build();
+
+        // Act
+        var result = await sut.RunAsync(CancellationToken.None);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task RunAsync_WhenSomeDeletesFail_ContinuesWithRemainingFiles()
+    {
+        // Arrange
+        var fileManager = Substitute.For<IFileManager>();
+        var exception = new IOException("File locked");
+        var sut = new DeleteTemporaryFilesBuilder()
+            .WithFileManager(fileManager)
+            .WithTemporaryFolder("C:\\Temp")
+            .WithFiles("C:\\Temp\\file1.txt", "C:\\Temp\\file2.txt", "C:\\Temp\\file3.txt")
+            .WithDeleteException("C:\\Temp\\file2.txt", exception)
+            .Build();
+
+        // Act
+        await sut.RunAsync(CancellationToken.None);
+
+        // Assert
+        fileManager.Received(1).Delete("C:\\Temp\\file1.txt");
+        fileManager.Received(1).Delete("C:\\Temp\\file2.txt");
+        fileManager.Received(1).Delete("C:\\Temp\\file3.txt");
+    }
+
+    [Fact]
+    public async Task RunAsync_CallsGetTemporaryFolder()
+    {
+        // Arrange
+        var fileManager = Substitute.For<IFileManager>();
+        var sut = new DeleteTemporaryFilesBuilder()
+            .WithFileManager(fileManager)
+            .WithTemporaryFolder("C:\\Temp")
+            .Build();
+
+        // Act
+        await sut.RunAsync(CancellationToken.None);
+
+        // Assert
+        fileManager.Received(1).GetTemporaryFolder();
+    }
+
+    [Fact]
+    public async Task RunAsync_CallsListFilesInDirectory()
+    {
+        // Arrange
+        var fileManager = Substitute.For<IFileManager>();
+        var sut = new DeleteTemporaryFilesBuilder()
+            .WithFileManager(fileManager)
+            .WithTemporaryFolder("C:\\Temp")
+            .Build();
+
+        // Act
+        await sut.RunAsync(CancellationToken.None);
+
+        // Assert
+        fileManager.Received(1).ListFilesInDirectory("C:\\Temp");
+    }
+}
