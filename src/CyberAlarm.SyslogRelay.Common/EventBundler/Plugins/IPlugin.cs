06630ec70ac90b87020dc5587e76a46e4dfca257
Waterstons <devops@waterstons.com>
2026-04-28 20:10:09 +0000

    Mirror from Azure DevOps [release=True] [version=1.0.78]

---
+++ src/CyberAlarm.SyslogRelay.Common/EventBundler/Plugins/IPlugin.cs
@@ -0,0 +1,15 @@
+﻿namespace CyberAlarm.SyslogRelay.Common.EventBundler.Plugins;
+
+/// <summary>
+/// Defines the contract for plugin implementations that provide event bundling algorithms.
+/// </summary>
+public interface IPlugin
+{
+    /// <summary>
+    /// Gets the name of the algorithm implemented by this plugin.
+    /// </summary>
+    /// <value>
+    /// A string representing the unique identifier or name of the bundling algorithm.
+    /// </value>
+    string AlgorithmName { get; }
+}
