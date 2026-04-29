4eed03d91fbab0eb0c55f3b1dd7c50f719cb4810
Waterstons <devops@waterstons.com>
2026-04-29 15:55:56 +0000

    Mirror from Azure DevOps [release=True] [version=1.0.103]

---
+++ src/CyberAlarm.SyslogRelay.Domain.Tests/Initialisation/CheckWriteAccessActivityTests.cs
@@ -0,0 +1,66 @@
+﻿using CyberAlarm.SyslogRelay.Domain.Extensions;
+using CyberAlarm.SyslogRelay.Domain.Services;
+using CyberAlarm.SyslogRelay.Domain.Tests;
+using CyberAlarm.SyslogRelay.Domain.Tests.Builders;
+
+namespace CyberAlarm.SyslogRelay.Domain.Tests.Initialisation;
+
+public sealed class CheckWriteAccessActivityTests
+{
+    private readonly CheckWriteAccessActivityBuilder _builder = new();
+
+    [Fact]
+    public async Task RunAsync_should_fail_when_platform_is_not_supported()
+    {
+        // Arrange
+        _builder.PlatformService
+            .GetPlatformType()
+            .Returns(PlatformType.NotSupported);
+
+        var unitUnderTest = _builder.Build();
+
+        // Act
+        var result = await unitUnderTest.RunAsync(CancellationToken.None);
+        // Assert
+
+        Assert.True(result.IsFailed);
+        Assert.Equal("Current platform is not supported.", result.ErrorMessage);
+    }
+
+    [Fact]
+    public async Task RunAsync_should_fail_when_volume_is_not_writable()
+    {
+        // Arrange
+        var dataPath = Guid.NewGuid().ToString();
+
+        _builder.FileManager
+            .GetDataPath()
+            .Returns(dataPath);
+
+        _builder.FileManager
+            .CanWriteFile(Arg.Any<string>())
+            .Returns(false);
+
+        var unitUnderTest = _builder.Build();
+
+        // Act
+        var result = await unitUnderTest.RunAsync(CancellationToken.None);
+
+        // Assert
+        Assert.True(result.IsFailed);
+        Assert.Equal($"Data volume '{dataPath}' is not writable.", result.ErrorMessage);
+    }
+
+    [Fact]
+    public async Task RunAsync_should_succeed_when_write_access_checks_pass()
+    {
+        // Arrange
+        var unitUnderTest = _builder.Build();
+
+        // Act
+        var result = await unitUnderTest.RunAsync(CancellationToken.None);
+
+        // Assert
+        Assert.True(result.IsSuccess);
+    }
+}
