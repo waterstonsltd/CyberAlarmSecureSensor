3481154e932c1a584cff2d077d3eb871549d2144
Waterstons <devops@waterstons.com>
2026-04-29 14:38:14 +0000

    Mirror from Azure DevOps [release=True] [version=1.0.100]

---
+++ src/CyberAlarm.SyslogRelay.Domain/Pipeline/PipelineOptions.cs
@@ -0,0 +1,22 @@
+﻿namespace CyberAlarm.SyslogRelay.Domain.Pipeline;
+
+public sealed class PipelineOptions
+{
+    public string AdditionalLocalSubnet { get; init; } = string.Empty;
+
+    public int ChannelCapacity { get; init; } = 10_000;
+
+    public int ParseFailureLogIntervalInMinutes { get; init; } = 60;
+
+    public int PersistenceBufferSize { get; init; } = 1_000;
+
+    public int PersistenceBufferIntervalInSeconds { get; init; } = 60;
+
+    public bool UploadRawLogs { get; init; }
+
+    public int PatternMatchingDegreeOfParallelism { get; init; } = Environment.ProcessorCount;
+
+    public int ParsingDegreeOfParallelism { get; init; } = Environment.ProcessorCount;
+
+    public int ValidationDegreeOfParallelism { get; init; } = Environment.ProcessorCount;
+}
