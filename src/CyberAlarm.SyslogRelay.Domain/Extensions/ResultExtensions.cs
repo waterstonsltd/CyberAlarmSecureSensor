7439c7308359b083a243cdc99cd62b7fa386bed1
Waterstons <devops@waterstons.com>
2026-05-06 09:53:37 +0000

    Mirror from Azure DevOps [release=True] [version=1.0.110]

---
+++ src/CyberAlarm.SyslogRelay.Domain/Extensions/ResultExtensions.cs
@@ -0,0 +1,12 @@
+﻿using FluentResults;
+
+namespace CyberAlarm.SyslogRelay.Domain.Extensions;
+
+public static class ResultExtensions
+{
+    extension<TResult>(TResult result)
+        where TResult : ResultBase<TResult>
+    {
+        public string ErrorMessage => result.Errors.Single().Message;
+    }
+}
