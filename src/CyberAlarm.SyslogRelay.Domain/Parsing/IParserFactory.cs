1eeac939a123bda6e36e0d5c464971465d77359a
Waterstons <devops@waterstons.com>
2026-05-07 11:25:35 +0000

    Mirror from Azure DevOps [release=True] [version=1.0.112]

---
+++ src/CyberAlarm.SyslogRelay.Domain/Parsing/IParserFactory.cs
@@ -0,0 +1,8 @@
+﻿using CyberAlarm.SyslogRelay.Common.Models;
+
+namespace CyberAlarm.SyslogRelay.Domain.Parsing;
+
+internal interface IParserFactory
+{
+    IParser? Create(string name, object? config);
+}
