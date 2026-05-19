c4ab87fdf53504077451edcd67e5883ea7b382ea
Waterstons <devops@waterstons.com>
2026-05-19 11:24:32 +0000

    Mirror from Azure DevOps [release=True] [version=1.0.127]

---
+++ src/CyberAlarm.SyslogRelay.Domain/Upload/Scheduling/StandardPeriodicTimer.cs
@@ -0,0 +1,20 @@
+﻿namespace CyberAlarm.SyslogRelay.Domain.Upload.Scheduling;
+
+public sealed class StandardPeriodicTimer(TimeSpan timeSpan) : IPeriodicTimer
+{
+    private readonly PeriodicTimer _actualTimer = new(timeSpan);
+
+    public async ValueTask<bool> WaitForNextTickAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            return await _actualTimer.WaitForNextTickAsync(cancellationToken);
+        }
+        catch (OperationCanceledException)
+        {
+            return false;
+        }
+    }
+
+    public void Dispose() => _actualTimer.Dispose();
+}
